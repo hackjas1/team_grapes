401006c78ba325fc59eea9ac062a7e2d61a5ee6c
hackjas1 <larosachristianpaul@gmail.com>
2026-08-24 20:20:50 +0800

    Enable debug mode and add fallback APP_KEY

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:co+u7hQ50c2EdK1TZxHyOp+rHEvbEPZ/91rp3HgzG3c='),
 
     'previous_keys' => [
         ...array_filter(
