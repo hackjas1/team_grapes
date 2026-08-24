440f6900dcf34738db74ca54b99c9c0ec8ea57de
hackjas1 <larosachristianpaul@gmail.com>
2026-08-24 20:29:22 +0800

    Auto-run migrations on container startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,5 +17,9 @@ php artisan config:clear || true
 php artisan route:clear || true
 php artisan view:clear || true
 
+# Run database migrations and seed default records automatically
+php artisan migrate --force || true
+php artisan db:seed --force || true
+
 # Execute main container command
 exec "$@"
